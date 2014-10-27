00652ad514bb3e19bd12ef856e8c3cd5a9ec9426
Allen Hurst <allen.hurst@ahurst.com>
2014-10-27 09:57:59 -0500

    Adding missing files

---
+++ OODTDD/OODTDD.Tests/RollDieTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using OODTDD;
+
+namespace OODTDD.Tests
+{
+    [TestFixture]
+    public class RollDieTests
+    {
+        private List<int> results;
+
+        [TestFixtureSetUp]
+        public virtual void SetUp()
+        {
+            var die = new Die();
+            results = die.SetUp1000000Results();
+        }
+
+
+        [Test]
+        public void ShouldRollAValidNumber()
+        {
+            foreach (var result in results)
+            {
+                Assert.That(result, Is.GreaterThan(0));
+                Assert.That(result, Is.LessThan(7));
+            }
+        }
+
+        [Test]
+        public void ShouldProduceAllNumbers()
+        {
+            for (int i = 1; i <= 6; i++)
+            {
+                Assert.IsTrue(results.Any(r => r == i), i + " not found");
+            }
+        }
+    }
+}
---
+++ OODTDD/OODTDD/Die.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OODTDD
+{
+    public class Die : IRollable
+    {
+        public int Roll()
+        {
+            var value = new Random().Next(1, 7);
+            FaceValue = value;
+            return value;
+        }
+
+        public int FaceValue { get; private set; }
+    }
+}
