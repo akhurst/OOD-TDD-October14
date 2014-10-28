51b28de924523a370832fb9a66ccda9bf03fe43a
brentmannarino <brent.mannarino@velir.com>
2014-10-28 11:27:41 -0400

    Adding StartingSquare to Board

--- OODTDD/OODTDD/Monopoly/Board.cs
+++ OODTDD/OODTDD/Monopoly/Board.cs
@@ -10,12 +10,18 @@ namespace OODTDD.Monopoly
         public void MoveToken(Token token, int spaces)
         {
             var startQuare = this.Squares.FirstOrDefault(x => x.HasToken(token));
+            
             int indexOf = this.Squares.IndexOf(startQuare);
 
             startQuare.RemoveToken(token);
 
             var endSquare = this.Squares[indexOf + spaces];
             endSquare.AddToken(token);
+        }
+
+        public Square StartingSquare()
+        {
+            return this.Squares.FirstOrDefault();
         }
     }
 }
